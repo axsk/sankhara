27fe75b6868f8a113a549a954c2fbc2138564601
Alex <smartmind23@googlemail.com>
2015-01-19 01:08:50 +0100

    communicate highlights between backgrond and contentscript

--- background.js
+++ background.js
@@ -1,20 +1,28 @@
-chrome.contextMenus.create({
-    "title": "Highlight this", 
-    "contexts":["selection"], 
-    "id":"highlight"
-})
-                                                                           
-function contextMenuHandler(info, tab) {
-    if (info.menuItemId == "highlight") {
-        chrome.tabs.sendMessage(
-            tab.id,
-            "highlight",
-            selectionResponse)
-    }   
+chrome.contextMenus.create({"title": "Highlight this", "contexts":["selection"], "id":"highlight"})
+chrome.contextMenus.onClicked.addListener( 
+    function(info, tab) {
+        if (info.menuItemId == "highlight")
+            chrome.tabs.sendMessage(tab.id, "highlightSelected", addHighlight)
+    })
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request == "getHighlights")
+        highlights = getHighlights(sender.tab.url)
+        sendResponse(highlights)
+  })
+
+testrange =   {start: {offset: 46, xpath:"/HTML[1]/BODY[1]/P[1]/text()[1]"}, end: {offset: 187, xpath: "/HTML[1]/BODY[1]/P[1]/text()[1]"}}
+  
+ranges = []
+ranges.push(testrange)
+
+function addHighlight(range) {
+    ranges.push(range)
 }
 
-function selectionResponse(response) {
-    
+function getHighlights(url) {
+    return ranges
 }
 
-chrome.contextMenus.onClicked.addListener(contextMenuHandler)
+
--- contentscript.js
+++ contentscript.js
@@ -1,16 +1,71 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if (request="highlight") {
-            highlightSelection()
-            sendResponse(selection.toString())
+        if (request=="highlightSelected") {
+        	selected = serializeSelection()        	
+            sendResponse(selected)
         }
     }
 )
 
-function highlightSelection() {
-    selection = window.getSelection()
-    range = selection.getRangeAt(0)
-    node = document.createElement("span")
+getHighlights()
+
+function getHighlights() {
+	// request highlights for current page
+	chrome.runtime.sendMessage("getHighlights", function(ranges) { updateHighlights(ranges) })
+}
+
+function serializeSelection() {
+	selection = window.getSelection()
+    return serializeRange(selection.getRangeAt(0))
+}
+
+function updateHighlights(ranges) {
+	// remove old highlight spans
+	// insert new highlight spans
+	node = document.createElement("span")
     node.style.backgroundColor = "yellow"
-    range.surroundContents(node)
+    for (var i=0, range; range = ranges[i]; i++) {
+		range = deserializeRange(range)
+		range.surroundContents(node)
+    }	
+}
+
+function serializeRange(range) {
+	return {start: {offset: range.startOffset, xpath:  getXPathForElement(range.startContainer)},
+	 	    end:   {offset: range.endOffset,   xpath:  getXPathForElement(range.endContainer)}   }
+}
+
+function deserializeRange(serializedRange) {
+	range = document.createRange()
+	range.setStart(lookupElementByXPath(serializedRange.start.xpath), serializedRange.start.offset)
+	range.setEnd  (lookupElementByXPath(serializedRange.end.xpath),   serializedRange.end.offset)
+	return range
+}
+
+function lookupElementByXPath(path) {
+    var evaluator = new XPathEvaluator(); 
+    var result = evaluator.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null); 
+    return  result.singleNodeValue; 
+} 
+
+function getXPathForElement(el) {
+	var xpath = '';
+	var pos, tmp;
+	while(el !== null && el.nodeType !== 9) {		
+		pos = 0;
+		tmp = el;
+		while(tmp) {
+			if (tmp.nodeType === el.nodeType && tmp.nodeName === el.nodeName) { // If it is ELEMENT_NODE of the same name
+				pos += 1;
+			}
+			tmp = tmp.previousSibling;
+		}
+		if (el.nodeType !== 3) 
+			xpath = '/'+el.nodeName+'['+pos+']' + xpath;
+		else
+			xpath = '/text()['+pos+']' + xpath;
+		
+		el = el.parentNode;
+	}
+	return xpath;
 }
